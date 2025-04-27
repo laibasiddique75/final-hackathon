e4b53963904fd37bdad570a2bd931e2a47005cfa
Laiba Siddique Qureshi <157726748+laibasiddique75@users.noreply.github.com>
2025-04-27 18:59:41 +0500

    add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://mern-femhack-production.up.railway.app' });
+const API = axios.create({ baseURL: 'https://final-hackathon-wypm.onrender.com/' });
 
 export default API;
